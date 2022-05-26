69da04c78cbd922940c65b1d667dde4785e49899
Johns-mx <johanfsk144@gmail.com>
2022-05-26 17:10:59 -0400

    TodoList added

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from './App';
 
-ReactDOM.render(<div>Hola mundo!!</div>, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
